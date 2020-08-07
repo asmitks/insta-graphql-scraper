334e08274635a0e301706f8efa4a5b3483fe3996
hotessy <divyanshus.ic@nsit.net.in>
2020-08-08 03:11:37 +0530

    added cursor restart

---
+++ README.md
@@ -0,0 +1,9 @@
+## Usage
+
+```
+python scraper.py [-h] --max MAX_DATE --min MIN_DATE --location LOCATION
+                  [--restore-cursor]
+```
+
+### Example
+ `python scraper.py --max "2020/07/15" --min "2020/06/15" --location "New-York-City" --restore-cursor`
--- scraper.py
+++ scraper.py
@@ -1,86 +1,111 @@
-import requests
-import time
+import argparse
+import codecs
+import json
+import logging
 import sys
-from metros import metro
+from pathlib import Path
+
+import backoff
+import requests
+
 import dateToId
-import os
-path='gdrive/My Drive/insta_data/'
-def pull_json(end_cursor):
-    try:
-        URL='https://www.instagram.com/explore/locations/204517928/chicago-illinois/?__a=1&max_id='+end_cursor
-        r = requests.get(url = URL) 
-        data = r.json() 
-        data=data['graphql']['location']['edge_location_to_media']
-        return data
-    except:
-        return None
 
-    
-def main(argv):
-    n=len(argv)
-    if(n<3):
-        print('pass 2 dates as input ..... and one location')
-        return
-    date1=argv[0]
-    date2=argv[1]
-    location=argv[2]
-        
-    try:
-        maxid1=dateToId.run(date1)
-        maxid2=dateToId.run(date2)
-    except:
-        print("error in date format, make sure no following zeroes example: 2020/7/15")
-        return
-    if(location not in metro):
-        print('Location not available in metros file')
-        return 
-    
-    
-    posts=[]
-    end_cursor=maxid1
-    if(n==4):
-        end_cursor=argv[3]
-    wrong=0
-    ctr=0
-    while(end_cursor and wrong<20 and end_cursor>maxid2):
-        try:
-            data=pull_json(end_cursor)
-        except:
-            print('waiting........')
-            wrong+=1
-            time.sleep(5)
-            continue
-        try:
-            posts.extend(data['edges'])
-        except:
-            print('waiting........ couldnt find edges in data make sure end_cursor is correct if passed')
-            wrong+=1
-            time.sleep(5)
-            continue
+logging.basicConfig(level=logging.DEBUG)
+METRO = json.load(open("./metros.json"))
+PATH = './data/'
+BASE_URL = "https://www.instagram.com/explore/locations/{location_id}/{location_name}/?__a=1&max_id={max_id}"
+MAX_TRIES = 11
 
-        end_cursor=data['page_info']['end_cursor']
-        with open(path+'cursor.txt', "w") as output:
-                    output.write(str(end_cursor))
-    #     break
-        if(len(posts)>=ctr):
-            ctr+=500
-            if(ctr>500):
-                os.remove(path+last)
-            with open(path+location+'_'+date1.replace('/','-')+'_'+date2.replace('/','-')+'_'+end_cursor+'.txt', "w") as output:
-                    output.write("posts="+str(posts))
-            last=location+'_'+date1.replace('/','-')+'_'+date2.replace('/','-')+'_'+end_cursor+'.txt'
-            
-        print(len(posts),end_cursor)
-    
-    if(wrong>=20):
-        print("Not Exited Succesfully IP BAN.. couldnt scrape more, scraped"+str(len(posts))+" posts ")
-        return
-    
-    with open(path+location+'_'+date1.replace('/','-')+'_'+date2.replace('/','-')+'_'+end_cursor+'.txt', "w") as output:
-                    output.write("posts="+str(posts))
-    
-    print("Done.... scraped "+str(len(posts))+" posts ")
-    
 
-if __name__=="__main__":
-    main(sys.argv[1:])
+@backoff.on_exception(wait_gen=backoff.fibo, max_tries=MAX_TRIES,
+                      exception=(requests.exceptions.HTTPError, requests.exceptions.ConnectionError))
+def pull_json(location_name, end_cursor):
+	try:
+		URL = BASE_URL.format(location_name=location_name, location_id=METRO[location_name],
+		                      max_id=end_cursor)
+		r = requests.get(url=URL)
+		if r.status_code == 200:
+			data = r.json()['graphql']['location']['edge_location_to_media']
+			return data
+		else:
+			return None
+	except:
+		return None
+
+
+def save_jsonl(data, dst='./'):
+	"""Saves the data to a jsonl file."""
+	path = Path(dst)
+	assert isinstance(data, list)
+	if path.exists():
+		f = codecs.open(dst, "ab", 'utf-32')
+	else:
+		path.parent.mkdir(parents=True, exist_ok=True)
+		f = codecs.open(dst, 'wb', 'utf-32')
+	f.writelines("%s\n" % json.dumps(s) for s in data)
+
+
+def scrape(date1, date2, location, restore_cursor=None):
+	try:
+		maxid1 = dateToId.run(date1)
+		maxid2 = dateToId.run(date2)
+	except:
+		logging.error("error in date format, make sure no following zeroes example: 2020/7/15")
+		sys.exit(1)
+
+	end_cursor = maxid1 if restore_cursor is None else restore_cursor
+	logging.debug("end_cursor: ", end_cursor)
+
+	while end_cursor and end_cursor > maxid2:
+		data = pull_json(location, end_cursor)
+		filename = f"{PATH}/{location}_{date1.replace('/', '-')}_{date2.replace('/', '-')}.jsonl"
+		save_jsonl(data['edges'], filename)
+		end_cursor = data['page_info']['end_cursor']
+		open(PATH + 'cursor.txt', "w").write(str(end_cursor))
+
+
+def main_old(argv):
+	assert len(argv) == 3, 'USAGE: python3 scraper.py <max_date> <min_date> <location>'
+	date1 = argv[0]
+	date2 = argv[1]
+	location = argv[2]
+
+	try:
+		maxid1 = dateToId.run(date1)
+		maxid2 = dateToId.run(date2)
+	except:
+		logging.error("error in date format, make sure no following zeroes example: 2020/7/15")
+		return
+
+	assert location in METRO, 'Location not available in metros.json'
+	scrape(maxid1, maxid2, location)
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--max", nargs=1, required=True)
+	parser.add_argument("--min", nargs=1, required=True)
+	parser.add_argument("--location", nargs=1, required=True)
+	parser.add_argument('--restore-cursor', action='store_true', default=False)
+
+	args = parser.parse_args()
+	print(args)
+
+	assert args.location[0] in METRO, 'Location not available in metros.json'
+
+	if args.restore_cursor:
+		logging.warning(f"--max {args.max} will be ignored. Scraping as far back as {args.min}")
+		maxid1 = open(PATH + 'cursor.txt', "r").read()
+	else:
+		logging.info(f"Scraping between dates {args.min[0]} and {args.max[0]}")
+
+	scrape(args.max[0], args.min[0], args.location[0])
+
+
+def test():
+	scrape("2020/07/16", "2020/07/15", "New-York-City")
+
+
+if __name__ == "__main__":
+	main()
+# test()
